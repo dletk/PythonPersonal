616bb5adbfe8a261fcdd861476f0d1d8f0b2c354
dletk <dle@macalester.edu>
2016-10-01 16:15:39 -0500

    Commit for Ferry problem

--- contest/ferryLoading.py
+++ contest/ferryLoading.py
@@ -13,29 +13,48 @@ def main():
             listCar.append((int(carInList[0]),carInList[1]))
         leftList = [i[0] for i in listCar if i[1] == "left" and i[0]<=length]
         rightList = [j[0] for j in listCar if j[1] == "right" and j[0]<=length]
+
         countLeft = 0
         sumL = 0
-        for index in range(len(leftList)):
-            sumL += leftList[index]
-            # print(sumL)
-            # print(length)
-            if sumL > length:
-                countLeft += 1
-                if index+1 >= len(leftList):
+        if len(leftList) == 1:
+            countLeft = 1
+        else:
+            for index in range(len(leftList)):
+                sumL += leftList[index]
+                if sumL > length:
                     countLeft += 1
+                    sumL = leftList[index]
+                if index+2 >= len(leftList):
+                    if leftList[index]+leftList[index+1] >= length:
+                        if leftList[index] >= length:
+                            countLeft += 2
+                        elif leftList[index+1] >= length:
+                            countLeft += 2
+                        else:
+                            countLeft += 2
+                        # countLeft += 3
                     break
-                sumL = leftList[index]
+
         countRight = 0
-        # print(countLeft)
         sumR = 0
-        for index in range(len(rightList)):
-            sumR += rightList[index]
-            if sumR > length:
-                countRight += 1
-                if index + 1 >= len(rightList):
+        if len(rightList) == 1:
+            countRight = 1
+        else:
+            for index in range(len(rightList)):
+                sumR += rightList[index]
+                if sumR > length:
                     countRight += 1
+                    sumR = rightList[index]
+                if index + 2 >= len(rightList):
+                    if rightList[index] + rightList[index + 1] >= length:
+                        if rightList[index] >= length:
+                            countRight += 2
+                        elif rightList[index + 1] >= length:
+                            countRight += 2
+                        else:
+                            countRight += 2
+                            # countLeft += 3
                     break
-                sumR = rightList[index]
         # print(countRight)
         if countLeft > countRight:
             print(2*(countLeft-1)+1)
