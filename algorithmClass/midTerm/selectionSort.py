d2fc63581bdbe177769aa5351a972e379bc2f1d3
Duc Le <duc.mtle@gmail.com>
2016-10-30 22:00:38 -0500

    Update selectionSort

--- algorithmClass/midTerm/selectionSort.py
+++ algorithmClass/midTerm/selectionSort.py
@@ -1,17 +1,24 @@
-def selectionSort(anArr):
-    for index in range(len(anArr)):
-        subArra = anArr[index:]
-        maxV = subArra[0]
+def randomNum():
+
+
+def selectionSort(inArr):
+
+    anArr = inArr
+    lenArr = len(anArr)
+
+    for index in range(lenArr):
+        maxV = anArr[0]
         index_max = 0
-        for index_2 in range(len(subArra)):
-            if subArra[index_2] > maxV:
+        for index_2 in range(0,lenArr-index):
+            if anArr[index_2] > maxV:
                 index_max = index_2
-                maxV = subArra[index_2]
-        temp_ele = anArr[-1]
-        anArr[-1] = maxV
-        anArr[index+index_max] = temp_ele
+                maxV = anArr[index_2]
+        temp_ele = anArr[lenArr-index-1]
+        anArr[lenArr-index-1] = maxV
+        anArr[index_max] = temp_ele
 
     return anArr
+
 
 if __name__ == '__main__':
     print(selectionSort([8,2,7,3,6,4,5,1]))
