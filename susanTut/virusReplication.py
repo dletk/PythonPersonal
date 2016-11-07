23d0b7a6888bc04632280d7e98046ad4edc975f4
Duc Le <duc.mtle@gmail.com>
2016-11-07 00:15:14 -0600

    Commit for finished virusReplication

--- susanTut/virusReplication.py
+++ susanTut/virusReplication.py
@@ -7,35 +7,60 @@ def getInput():
 def process(input_DNAs):
     original_DNA = input_DNAs[0]
     infected_DNA = input_DNAs[1]
-    front_diff = -1
-    end_diff = -1
-    for front, end, end_original, front_original in zip(range(len(infected_DNA)), range(len(infected_DNA)-1,-1,-1), range(len(original_DNA)-1,-1,-1), range(len(original_DNA))):
-        # print(front)
-        # print(end)
-        # print()
-        if front < len(original_DNA):
-            if front_diff == -1 and original_DNA[front] != infected_DNA[front]:
-                front_diff = front
-            if end_diff == -1 and original_DNA[end_original] != infected_DNA[end]:
-                end_diff = end
-            if front_diff != -1 and end_diff != -1:
+    length_ori = len(original_DNA)
+    length_inf = len(infected_DNA)
+#     Case 1: len orgininal == len infected
+    if length_ori == length_inf:
+        front = -1
+        end = -1
+        for index_front, index_end in zip(range(length_ori),range(length_ori-1,-1,-1)):
+            if original_DNA[index_front] != infected_DNA[index_front] and front == -1:
+                front = index_front
+            if original_DNA[index_end] != infected_DNA[index_end] and end == -1:
+                end = index_end
+            if end != -1 and front != -1:
                 break
-
-    print((front_diff,end_diff))
-    if front_diff == -1 and end_diff == -1:
-        if len(original_DNA) == len(infected_DNA):
+        if end == -1 and front == -1:
             return 0
         else:
-            return abs(len(original_DNA)-len(infected_DNA))
-    elif front_diff == end_diff:
-        if len(original_DNA) == len(infected_DNA):
-            return 1
+            return end - front + 1
+    # Case 2: len original > len_inf
+    elif length_ori > length_inf:
+        front = -1
+        end = -1
+        for index in range(length_inf):
+            if original_DNA[index] != infected_DNA[index]:
+                front = index
+                break
+        if front != -1:
+            new_inf = infected_DNA[:front:-1]
+            for index in range(len(new_inf)):
+                if new_inf[index] != original_DNA[length_ori-index-1]:
+                    end = length_inf - index - 1
+                    break
+        if front == -1 or end == -1:
+            return 0
         else:
-            return abs(len(original_DNA)-len(infected_DNA))
-    elif front_diff == 0:
-        return abs(end_diff-front_diff)
+            return end - front + 1
+#     Case 3: len ori < len inf
     else:
-        return abs(end_diff-front_diff)+1
+        front = -1
+        end = -1
+        for index in range(length_ori):
+            if original_DNA[index] != infected_DNA[index]:
+                front = index
+                break
+        if front != -1:
+            new_ori = original_DNA[:front:-1]
+            for index in range(len(new_ori)):
+                if new_ori[index] != infected_DNA[length_inf-index-1]:
+                    end = length_ori-index-1
+                    break
+        if front == -1 or end == -1:
+            return length_inf - length_ori
+        elif end != -1:
+            return length_inf - length_ori + (end - front + 1)
+
 
 if __name__ == '__main__':
     result = process(getInput())
