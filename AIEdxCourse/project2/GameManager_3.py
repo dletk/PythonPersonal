4c78e34da4635eb616efdabae0d9f25887eaa469
dletk <dle@macalester.edu>
2017-12-20 16:54:45 -0600

    Using NN to play 2048

--- AIEdxCourse/project2/GameManager_3.py
+++ AIEdxCourse/project2/GameManager_3.py
@@ -38,8 +38,8 @@ class GameManager:
         self.displayer = None
         self.over = False
         # Load the DNN model
-        # self.model_NN = load_model("all_2048_8000games.h5")
-        self.result_file = open("resultRandom.txt", "a")
+        self.model_NN = load_model("all_2048_8000games.h5")
+        # self.result_file = open("resultRandom.txt", "a")
 
     def setComputerAI(self, computerAI):
         self.computerAI = computerAI
@@ -79,19 +79,20 @@ class GameManager:
 
             if turn == PLAYER_TURN:
                 print("Player's Turn:", end="")
-                # Using NN to make move instead
+
                 # move = self.playerAI.getMove(gridCopy)
 
-                # list_proba = self.model_NN.predict(self.generateCurrentBoard1D(self.grid.map))[0].tolist()
+                # Using NN to make move instead
+                list_proba = self.model_NN.predict(self.generateCurrentBoard1D(self.grid.map))[0].tolist()
+                move = list_proba.index(max(list_proba))
 
-                # move = list_proba.index(max(list_proba))
-                availableMoves = self.grid.getAvailableMoves()
-                move = availableMoves[randint(0, len(availableMoves)-1)]
+                # availableMoves = self.grid.getAvailableMoves()
+                # move = availableMoves[randint(0, len(availableMoves)-1)]
                 while not self.grid.canMove([move]):
                     # print(list_proba)
-                    # list_proba[move] = -1
-                    # move = list_proba.index(max(list_proba))
-                    randint(0,3)
+                    list_proba[move] = -1
+                    move = list_proba.index(max(list_proba))
+                    # randint(0,3)
                 print(actionDic[move])
 
                 # Validate Move
@@ -123,16 +124,16 @@ class GameManager:
             """
             Comment this out if do not want to have the display
             """
-            # if not self.over:
-            #     self.displayer.display(self.grid)
+            if not self.over:
+                self.displayer.display(self.grid)
 
             # Exceeding the Time Allotted for Any Turn Terminates the Game
             self.updateAlarm(time.clock())
 
             turn = 1 - turn
         print(maxTile)
-        self.result_file.write(str(maxTile)+"\n")
-        self.result_file.close()
+        # self.result_file.write(str(maxTile)+"\n")
+        # self.result_file.close()
 
     def isGameOver(self):
         return not self.grid.canMove()
